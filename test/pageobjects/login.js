ad9913938c20fcbd0015e4fa28042cbb069e5b05
audreyChecketts <achkts@gmail.com>
2024-11-13 10:07:17 -0700

    creating a seperate file for the passwords etc

--- test/pageobjects/login.js
+++ test/pageobjects/login.js
@@ -2,6 +2,7 @@ import { $ } from '@wdio/globals'
 import { expect } from '@wdio/globals'
 import SauceBasePage from './basePage.js';
 import Security from '../pageobjects/security.js';
+import users from '../pageobjects/users.js';
 
 
 class Login extends SauceBasePage {
@@ -26,13 +27,7 @@ class Login extends SauceBasePage {
         return $('#logout_sidebar_link');
     }
 
-    users = [{username: 'standard_user', password: 'secret_sauce'}, 
-        {username: 'locked_out_user', password: 'secret_sauce'},
-        {username: 'problem_user', password: 'secret_sauce'},
-        {username: 'performance_glitch_user', password: 'secret_sauce'},
-        {username: 'error_user', password: 'secret_sauce'},
-        {username: 'visual_user', password: 'secret_sauce'}
-    ];
+
 
     async multiLogin (username, password){
 
@@ -43,6 +38,7 @@ class Login extends SauceBasePage {
             await this.inputPassword.setValue(password);
             await this.btnSubmit.click();
             await expect(Security.HomePage).toBeExisting;
+           
         }
 
         //specific to the locked out username
@@ -62,6 +58,7 @@ class Login extends SauceBasePage {
             await expect(Security.flashAlert2).toHaveText(
                   expect.stringContaining('Epic sadface: Username and password do not match any user in this service'))
         }
+
         
 }
 
@@ -70,10 +67,9 @@ async testMultiLogin() {
     for (let user of this.users) {
         console.log(`Testing with username: ${user.username}`);
         await this.multiLogin(user.username, user.password);
-        await this.hamburgerMenu.click();
-        await this.logOut.click();
         
     }
+    
 }
 
     openBasePage () {
