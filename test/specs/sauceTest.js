8d8deaa5a4eec51d99a0f8d74b751c268dbd4fe3
audreyChecketts <achkts@gmail.com>
2024-11-13 07:46:32 -0700

    change test names functions and lines and pause

--- test/specs/sauceTest.js
+++ test/specs/sauceTest.js
@@ -8,25 +8,22 @@ import Security from '../pageobjects/security.js';
 describe('My seccessful Login application', () => {
         it('should login with valid username', async () => {
             await Login.openBasePage();
-   
             await Login.positiveLogin('standard_user', 'secret_sauce');
-
             await expect(Security.HomePage).toBeExisting;
             
+            await browser.pause(3000);
 
             await Login.openBasePage();
-
             await Login.negativeLoginName('locked_out_user', 'secret_sauce');
-
             await expect(Security.flashAlert1).toBeExisting();
             await expect(Security.flashAlert1).toHaveText(
                 expect.stringContaining('Epic sadface: Sorry, this user has been locked out.')
             );
 
+            await browser.pause(3000);
+
             await Login.openBasePage();
-
-            await Login.negativeLoginName('standard_user', 'asdfwoij');
-
+            await Login.negativeLogin('standard_user', 'asdfwoij');
             await expect(Security.flashAlert2).toBeExisting();
             await expect(Security.flashAlert2).toHaveText(
                   expect.stringContaining('Epic sadface: Username and password do not match any user in this service')
